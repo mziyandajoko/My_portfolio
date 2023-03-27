4d1455ebe1d0bd8cbf41aae7d488b66209ba2609
Mziyanda Joko <joko.mzee@gmail.com>
2023-03-27 14:31:26 +0200

    adds hero section for landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
+import '../../css/index.css'
 
+const Title = styled.h2`
+font-size:  5rem;
+color: var(--clr-primary);
+font-family: var(--sans)
+
+
+`
 export default () => (
   <div>
-    <section id="intro">
-  <p class="name">Hi, my name is <span>Mziyanda Joko.</span></p>
-  <h2>I DO web developer.</h2>
-  <p>I'm a Junior web developer specializing in REACT, FLUTTER & WordPress.</p>
-  <p>Currently, I'm looking looking for Work <a href="#" target="_blank">Resume</a>.</p>
-</section>
+    <section  class="hero">
+      <p class="name">Hi, my name is <span>Mziyanda Joko.</span></p>
+      <Title>Web Developer.</Title>
+      <p>I'm a Junior web developer specializing in REACT, FLUTTER & WordPress.</p>
+      <p>Currently, I'm looking looking for Work <a href="https://drive.google.com/file/d/13zQrR7VXgGdh6wOHJL3lQ674F-34siV7/view?usp=sharing" target="_blank">Resume</a>.</p>
+    </section>
   </div>
 )
